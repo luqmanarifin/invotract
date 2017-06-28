e0cb215b6ad855721b0f58637be740c6c0b3652f
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-06-28 21:15:52 +0900

    add new modules skeleton

---
+++ src/jp/co/multibook/invotract/pdf2xml/PdfToXml.java
@@ -0,0 +1,20 @@
+package jp.co.multibook.invotract.pdf2xml;
+
+/**
+ * Created by luqmanarifin on 6/28/17.
+ */
+public class PdfToXml {
+
+  public PdfToXml() {
+
+  }
+
+  /**
+   *
+   * @return path to the result XML file
+   */
+  public static String getXmlOcr(String filePath) {
+    return null;
+  }
+
+}
---
+++ src/jp/co/multibook/invotract/xml2sentence/XmlToSentence.java
@@ -0,0 +1,23 @@
+package jp.co.multibook.invotract.xml2sentence;
+
+/**
+ * Created by luqmanarifin on 6/28/17.
+ */
+public class XmlToSentence {
+
+  public XmlToSentence() {
+
+  }
+
+  /**
+   *
+   * @param filePath
+   * @return path to the found sentence of the document, using format
+   * X Y size sentence
+   * one line per sentence
+   */
+  public String getSentence(String filePath) {
+    return null;
+  }
+
+}
